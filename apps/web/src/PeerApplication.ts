9b3b82c083ba826144d93460404f3784b05cab11
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2022-11-28 02:20:24 +0200

    refactor: remove references

--- apps/web/src/PeerApplication.ts
+++ apps/web/src/PeerApplication.ts
@@ -1,3 +1,4 @@
+import { PeerApplication, PeerjsClient } from "@cenkce/p2p-router";
 
 const [PeerService, Controller, Listener] = PeerApplication(new PeerjsClient({ path: "/teachers", port: 9000, host: "localhost" }));
 
